33c5be2f9b75f9d96896261b4715fc3ca91805ee
Monko2k <kevinhuang2k@gmail.com>
2022-01-03 17:55:45 -0500

    fast enough implementation

--- scrape.py
+++ scrape.py
@@ -2,12 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import json
 import re
 def getPool(URL):
-    driver = webdriver.Chrome('./chromedriver.exe')
+    options = Options()
+    options.add_argument('--host-resolver-rules=MAP osu.ppy.sh 127.0.0.1:9876')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options) 
     driver.get(URL)
-    id = 'test'
+    driver.execute_cdp_cmd('Network.setBlockedURLs', {"urls": ["osu.ppy.sh"]})
+    driver.execute_cdp_cmd('Network.enable', {})
+    reg = re.search('[^/]+$', URL)
+    id = reg.group(0)
+    print(f"Processing {id}")
     _ = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.TAG_NAME, 'mat-row')))
     pool = {}
     pool['name'] = driver.find_element(By.TAG_NAME, 'mat-card-title').text
@@ -18,21 +27,21 @@ def getPool(URL):
     pool['maps']['DT'] = []
     pool['maps']['FM'] = []
     pool['maps']['TB'] = []
+    mods = []
     rows = driver.find_elements(By.TAG_NAME, 'mat-row')
     for row in rows:
-        mod = row.find_element(By.XPATH, 'mat-cell[1]').text
+        mods.append(row.find_element(By.XPATH, 'mat-cell[1]').text)
         row.click()
+    for ind, mod in enumerate(mods):
         driver.switch_to.window(driver.window_handles[-1])
         beatmap = driver.current_url
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
         reg = re.search('\d+$', beatmap)
         pool['maps'][mod[:2]].append({
             'id': reg.group(0)
         })
-
-    driver.close()
-    with open('./pools/' + id + '.json', 'w') as output:
+        driver.close()
+    driver.quit()
+    with open('./pools/' + id + '.json', 'w+') as output:
         json.dump(pool, output)
 
 def main():
