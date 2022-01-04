fd7dc5f07ae893d4719df821b4239b311297eef0
Monko2k <kevinhuang2k@gmail.com>
2022-01-03 19:51:31 -0500

    change datastructure

--- scrape.py
+++ scrape.py
@@ -6,49 +6,52 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import json
 import re
-def getPool(URL):
-    options = Options()
-    options.add_argument('--host-resolver-rules=MAP osu.ppy.sh 127.0.0.1:9876')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options) 
+def getPool(URL, driver):
     driver.get(URL)
-    driver.execute_cdp_cmd('Network.setBlockedURLs', {"urls": ["osu.ppy.sh"]})
-    driver.execute_cdp_cmd('Network.enable', {})
     reg = re.search('[^/]+$', URL)
     id = reg.group(0)
     print(f"Processing {id}")
     _ = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.TAG_NAME, 'mat-row')))
     pool = {}
+    modgroups = {}
     pool['name'] = driver.find_element(By.TAG_NAME, 'mat-card-title').text
-    pool['maps'] = {}
-    pool['maps']['NM'] = []
-    pool['maps']['HD'] = []
-    pool['maps']['HR'] = []
-    pool['maps']['DT'] = []
-    pool['maps']['FM'] = []
-    pool['maps']['TB'] = []
+    pool['modgroups'] = []
+    modgroups['NM'] = []
+    modgroups['HD'] = []
+    modgroups['HR'] = []
+    modgroups['DT'] = []
+    modgroups['FM'] = []
+    modgroups['TB'] = []
     mods = []
     rows = driver.find_elements(By.TAG_NAME, 'mat-row')
     for row in rows:
         mods.append(row.find_element(By.XPATH, 'mat-cell[1]').text)
         row.click()
-    for ind, mod in enumerate(mods):
+    for mod in mods:
         driver.switch_to.window(driver.window_handles[-1])
         beatmap = driver.current_url
         reg = re.search('\d+$', beatmap)
-        pool['maps'][mod[:2]].append({
-            'id': reg.group(0)
-        })
+        modgroups[mod[:2]].append(reg.group(0))
         driver.close()
-    driver.quit()
+    driver.switch_to.window(driver.window_handles[0])
+    driver.close()
+    for group in modgroups:
+        pool['modgroups'].append({
+            "mod": group,
+            "maps": modgroups[group]
+        })
     with open('./pools/' + id + '.json', 'w+') as output:
         json.dump(pool, output)
 
 def main():
     with open('./pools.txt', 'r') as input:
         lines = input.readlines()
+    options = Options()
+    options.add_argument('--host-resolver-rules=MAP osu.ppy.sh 127.0.0.1:9876')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options) 
     for line in lines:
-        getPool(line.strip())
+        getPool(line.strip(), driver)
 
 if __name__ == "__main__":
     main()
